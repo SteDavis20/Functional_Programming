f50c49a3515828c528bca131efc4f8172c94134f
Stephen Davis <stephen.davis2016@gmail.com>
2021-11-13 14:46:41 +0000

    Uploading tutorial2 and start of tutorial3 solution

--- Tutorials/tutorial2.hs
+++ Tutorials/tutorial2.hs
@@ -1,10 +1,39 @@
+module Tut2 where
+import Data.Char (toUpper)
+
+name, idno, username :: String
+name      =  "Stephen, Davis"  -- replace with your name
+idno      =  "18324401"    -- replace with your student id
+username  =  "stdavis"   -- replace with your TCD username
+
+
+declaration -- do not modify this
+ = unlines
+     [ ""
+     , "@@@ This exercise is all my own work."
+     , "@@@ Signed: " ++ name
+     , "@@@ "++idno++" "++username
+     ]
+
+
 {- Part 1
 
 Write `conc` to concatenate lists
 
 -}
 conc :: [a] -> [a] -> [a]
-conc = undefined
+conc [] a = a
+conc a [] = a
+conc a b = a ++ b
+
+{- ++ function below is provided by Haskell Prelude
+
+(++) :: [a] -> [a] -> [a]
+[] ++ [a] = [a]
+[a] ++ [] = [a]
+(x:xs) ++ ys = x : (xs ++ ys) 
+
+-}
 
 {- Part 1
 
@@ -12,4 +41,5 @@ Write `rev` to reverse lists
 
 -}
 rev :: [a] -> [a]
-rev xs = undefined
+rev [] = []
+rev (a:as) = rev as ++ [a] 
--- Tutorials/tutorial3.hs
+++ Tutorials/tutorial3.hs
@@ -6,14 +6,23 @@ data BinTree a
 {- Part 1
 Write `binsert` to insert items into a BinTree
 -}
-binsert = error "binsert NYI!"
+binsert value Empty = Node Empty value Empty 
+binsert value (Node leftSubtree currentValue rightSubtree)  
+  | value < currentValue = Node (binsert value leftSubtree) currentValue rightSubtree
+  | value == currentValue = Node leftSubtree value rightSubtree
+  | value > currentValue = Node leftSubtree currentValue (binsert value rightSubtree)  
 
 {- Part 2
-Write `blookup` to search a BinTree
+Write `blookup` to search a BinTree to see if value exists
 -}
-blookup = error "blookup NYI!"
+blookup desiredVal Empty = False
+blookup desiredVal (Node leftSubtree value rightSubtree)
+  | desiredVal < value = blookup desiredVal leftSubtree
+  | desiredVal > value = blookup desiredVal rightSubtree
+  | desiredVal == value = True
 
 {- Part 3
 Write `tpose` to transpose a list of lists
 -}
-tpose = error "tpose NYI!"
+tpose :: [[a]] -> [[a]]
+???
