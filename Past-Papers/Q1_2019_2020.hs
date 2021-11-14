2e0eb01ffe81d23de7a85326b6b08c1419af3699
Stephen Davis <stephen.davis2016@gmail.com>
2021-11-14 15:54:54 +0000

    Uploading answers to first few questions

--- Past-Papers/Q1_2019_2020.hs
+++ Past-Papers/Q1_2019_2020.hs
@@ -2,16 +2,29 @@ module Q1_2019_2020 where
 
 -- Q1)
 
+--    returns 1st element in a non-empty list, error otherwise
 -- a) head :: [a] -> a
 
+head :: [a] -> a
+head (x:xs) = x      
 
+-- return list with last element removed
+-- using name myInit so compiler doesn't complain that init already exists in Prelude.
 -- b) init :: [a] -> [a]
 
+myInit :: [a] -> [a]
+myInit [x] = []
+myInit (_:xs) = myInit xs
 
 
 -- c) Concatenate 2 lists together
 
 -- (++) :: [a] -> [a] -> [a]
+
+myPlusPlus :: [a] -> [a] -> [a]
+myPlusPlus [] a = a
+myPlusPlus a [] = a 
+myPlusPlus (x:xs) ys = x : myPlusPlus xs ys 
 
 
 -- d) replicate n x returns a list containing n copies of x.
@@ -31,3 +44,6 @@ module Q1_2019_2020 where
 -- f) Compute the max of a non-empty list
 
 -- maximum :: Ord a => [a] -> a
+myMaximum :: [a] -> a
+myMaximum [a] = a
+-- myMaximum (x:xs)
